9d8886fcbcaf8cdd7c9637f44ce060223f939ffc
R3tr0LastKnight <shubh14shahu@gmail.com>
2025-08-03 04:34:08 +0530

    vercel xxx

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,4 +1,20 @@
-const serverless = require("serverless-http");
-const app = require("../index"); // Import your Express app
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+require("./database/db");
 
-module.exports = serverless(app);
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+// Route
+const spotifyRoutes = require("./routes/spotify");
+app.use("/api/spotify", spotifyRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to The MCG backend");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

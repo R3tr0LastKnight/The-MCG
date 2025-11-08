e8604d1e710f63087259f02b1264f3f22ee7cc3d
R3tr0LastKnight <shubh14shahu@gmail.com>
2025-11-08 17:31:34 +0530

    08/11 - errors fixes 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Layout from "./Layout";
 // import { useAuth } from "./context/AuthContext";
